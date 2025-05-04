a4ec1b3e4593da5e3a1d56a43dd3c56ff8af67d3
Babar <babar.javed630@gmail.com>
2025-05-05 01:33:06 +0500

    fix entdpoint convention

--- server/routes/todo.routes.js
+++ server/routes/todo.routes.js
@@ -1,14 +1,14 @@
 const todoController = require("../controller/todo.controller.js.js");
 
 const routes = (app) => {
-  app.get("/todo/", todoController.getAllTodos);
-  app.get("/todo/:id", todoController.getTodo);
+  app.get("/todos/", todoController.getAllTodos);
+  app.get("/todos/:id", todoController.getTodo);
 
-  app.post("/todo/", todoController.postTodo);
-  app.patch("/todo/:id", todoController.patchTodo);
+  app.post("/todos/", todoController.postTodo);
+  app.patch("/todos/:id", todoController.patchTodo);
 
-  app.delete("/todo/", todoController.deleteAllTodos);
-  app.delete("/todo/:id", todoController.deleteTodo);
+  app.delete("/todos/", todoController.deleteAllTodos);
+  app.delete("/todos/:id", todoController.deleteTodo);
 };
 
 module.exports = routes;
